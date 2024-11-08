4a9983f6825f7f3fe29aab62527cd0ca18866b37
Zhakhongir Rakhmankulov <zhakhongir.r@bilimland.com>
2024-11-08 22:56:06 +0500

    [DOT-368] - fix outputs to use indexed access for openstack_compute_volume_attach_v2 attributes;

--- 020-outputs.tf
+++ 020-outputs.tf
@@ -22,25 +22,25 @@ output "volume_type" {
 # Outputs for openstack_compute_volume_attach_v2
 output "attachment_ids" {
   description = "List of IDs of the volume attachments"
-  value       = [for va in openstack_compute_volume_attach_v2.va : va.id]
+  value       = [for idx in range(length(openstack_compute_volume_attach_v2.va)) : openstack_compute_volume_attach_v2.va[idx].id]
 }
 
 output "attached_instance_ids" {
   description = "List of IDs of the instances to which the volumes are attached"
-  value       = [for va in openstack_compute_volume_attach_v2.va : va.instance_id]
+  value       = [for idx in range(length(openstack_compute_volume_attach_v2.va)) : openstack_compute_volume_attach_v2.va[idx].instance_id]
 }
 
 output "attached_volume_ids" {
   description = "List of IDs of the attached volumes"
-  value       = [for va in openstack_compute_volume_attach_v2.va : va.volume_id]
+  value       = [for idx in range(length(openstack_compute_volume_attach_v2.va)) : openstack_compute_volume_attach_v2.va[idx].volume_id]
 }
 
 output "attached_devices" {
   description = "List of device paths for the attached volumes (depends on the hypervisor)"
-  value       = [for va in openstack_compute_volume_attach_v2.va : va.device]
+  value       = [for idx in range(length(openstack_compute_volume_attach_v2.va)) : openstack_compute_volume_attach_v2.va[idx].device]
 }
 
 output "multiattach_enabled_list" {
   description = "List indicating if multiattach is enabled for each volume attachment"
-  value       = [for va in openstack_compute_volume_attach_v2.va : va.multiattach]
+  value       = [for idx in range(length(openstack_compute_volume_attach_v2.va)) : openstack_compute_volume_attach_v2.va[idx].multiattach]
 }
